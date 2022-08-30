fd36e7848263c2316199fab6114b30a9998447a1
alamin09876 <alamin.ahmed0795@gmail.com>
2022-08-30 17:08:54 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ const displayPhones = (phones, datalimit) =>{
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <button onclick = "phoneDitals('${phone.slug}')" href="#" class="btn btn-primary">Show details</button>
+          <button onclick = "phoneDitals('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneModal">Show details</button>
         </div>
       </div>
         `
@@ -77,7 +77,19 @@ const phoneDitals = async id =>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data)
+    photeDetails(data.data)
 
 }
-// loadPhones();
+
+const photeDetails = phone =>{
+    console.log(phone)
+    const phoneModal = document.getElementById('phoneModalLabel');
+    phoneModal.innerText = phone.name;
+    const relaseDate = document.getElementById('realise-date');
+    relaseDate.innerHTML = `
+    <p>Release Date : ${phone.releaseDate ? phone.releaseDate : "no relase date found"}</p>
+    <p>Release Date : ${phone.mainFeatures.displaySize}</p>
+    <p>Release Date : ${phone.mainFeatures.storage}</p>
+    `
+}
+// loadPhones('apple');
